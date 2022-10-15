576cac91b0d20cb937f41ee2d03ddd0b2e835f61
David T <matkatdavid@gmail.com>
2022-10-14 20:30:10 -0600

    Can successfully generate cards from json

--- js/cards.js
+++ js/cards.js
@@ -1,5 +1,45 @@
 import DataService from "./DataService.js";
-const myDataService = new DataService();
-const url = "../json/wdd.json";
 
-const content = await myDataService.getContent(url);
+const myDataService = new DataService();
+
+// Need function that can figure out which page is loading from the URL string
+// This will determine which json file to pull from.
+
+function buildPage(content) {
+  let html = "";
+  console.log(typeof content);
+  content.forEach((element) => {
+    html += `
+    <div class="card">
+      <img
+        src="${element.img}"
+        alt="${element.alt}"
+      />
+      <div class="card-text">
+        <h3>${element.title}</h3>
+        <p>${element.date}</p>
+      </div>
+    <a href="./project?=${element.slug}" class="card-button">See More</a>
+  </div>`;
+  });
+
+  document.querySelector("#card-grid").innerHTML = html;
+}
+
+window.addEventListener("load", async function () {
+  let url = "../json/";
+  const address = window.location.pathname;
+  if (address.search("/wdd") != -1) {
+    url += "wdd.json";
+  } else if (address.search("/se") != -1) {
+    url += "se.json";
+  } else if (address.search("/other") != -1) {
+    url += "other.json";
+  } else if (address.search("/photo") != -1) {
+    url += "photo.json";
+  }
+  console.log(url);
+  const content = await myDataService.getContent(url);
+  console.log(content);
+  buildPage(content);
+});
---
+++ js/project.js
@@ -0,0 +1,4 @@
+import DataService from "./dataService.js";
+import utils from "./utils.js";
+const { getParams, clickMenu } = utils;
+const myDataService = new DataService();
